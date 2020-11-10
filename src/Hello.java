21f3fae281709a12891d19489b272e38afec27c8
Dean Wynn <wynnkhalifa117@gmail.com>
2020-11-10 14:42:35 -0600

    Added Class and new lines

--- src/Hello.java
+++ src/Hello.java
@@ -1,7 +1,6 @@
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("Hello ");
-        // line 1
+        System.out.println("Hello");
         // line 2
         // line 3
     }
